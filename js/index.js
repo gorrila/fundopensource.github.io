86fd731ba1a3889229e8945c46435487d6c038c6
Daniel Foré <daniel@elementary.io>
2017-08-13 12:58:53 -0700

    Add all the stuff

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$.getJSON('/data/distros.json', function (json) {
+    for (var key in json) {
+        var distro = json[key];
+
+        var card = '<a class="card ' + key + '" href="' + distro.address + '" target="_blank"></a>';
+        $('.deck.distro').append (card);
+
+        var image_header = '<img class="image-header" src="/images/logos/' + key + '.svg"></img>';
+        $('.card.' + key).append (image_header);
+
+        var content_area = '<div class="content-area"></div>';
+        $('.card.' + key).append (content_area);
+
+        var title = '<h1>' + distro.title + '</h1>';
+        $('.' + key +' .content-area').append (title);
+    }
+});
+
+$.getJSON('/data/apps.json', function (json) {
+    for (var key in json) {
+        var app = json[key];
+
+        var card = '<a class="card ' + key + '" href="' + app.address + '" target="_blank"></a>';
+        $('.deck.apps').append (card);
+
+        var image_header = '<img class="image-header" src="/images/logos/' + key + '.svg"></img>';
+        $('.card.' + key).append (image_header);
+
+        var content_area = '<div class="content-area"></div>';
+        $('.card.' + key).append (content_area);
+
+        var title = '<h1>' + app.title + '</h1>';
+        $('.' + key +' .content-area').append (title);
+    }
+});
